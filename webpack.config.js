0176b58ea53010b87eae9c5907880230010b1d49
1358872096@qq.com <1358872096@qq.com>
2020-10-17 15:59:39 +0800

    feat: assets management

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
 
-module.export = {
+console.log('webpack build start...');
+module.exports = {
 	entry: './src/index.js',
 	output: {
-		filename: 'main.js',
+		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+				],
+			},
+		],
 	},
 };
